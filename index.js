46fc510798327a94bc9e0bac8a608b67e1930934
Prykhodko Dmytro <prykhodko.dmytro1995@gmail.com>
2021-05-30 14:06:34 +0300

    load

---
+++ index.js
@@ -0,0 +1,12 @@
+// Change code below this line
+
+function add(a ,b ,c ) {
+ const result = a + b + c ;
+  console.log(`Addition result equals ${result}`);
+  
+    // Change code above this line
+}
+add(a , b, c);
+add(15, 27, 10);
+add(10, 20, 30);
+add(5, 10, 15);
